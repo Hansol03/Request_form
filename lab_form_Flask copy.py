8f76a95f7ad0ece252eb046b3986c8fe051b211f
Hansol03 <hansol.lee0323@gmail.com>
2023-06-08 11:22:16 +0900

    edited

--- lab_form_Flask copy.py
+++ lab_form_Flask copy.py
@@ -5,7 +5,7 @@ import openpyxl
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'stl', 'step', 'stp', 'f3d', 'obj'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -26,24 +26,28 @@ def index():
       background-color: #f2f2f2;
       margin: 0;
       padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 140vh;
+    }
+
+    /* 폼 컨테이너 스타일 */
+    .form-container {
+      width: 500px;
+      padding: 30px;
+      background-color: #f9f9f9;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     }
 
     /* 제목 스타일 */
     h1 {
       color: #333;
       font-size: 24px;
-      margin-top: 30px;
-    }
-
-    /* 폼 컨테이너 스타일 */
-    .form-container {
-      max-width: 500px;
-      margin: 0 auto;
-      padding: 30px;
-      background-color: #f9f9f9;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      margin-bottom: 30px;
+      text-align: center;
     }
 
     /* 입력 필드 스타일 */
@@ -75,6 +79,7 @@ def index():
       cursor: pointer;
       font-size: 16px;
       margin-top: 20px;
+      width: 100%;
     }
 
     input[type="submit"]:hover {
@@ -95,8 +100,8 @@ def index():
   </style>
 </head>
 <body>
-  <h1>양식 작성</h1>
   <div class="form-container">
+    <h1>양식 작성</h1>
     <form method="post" enctype="multipart/form-data" action="/submit">
       <label for="width">가로 길이:</label>
       <input type="text" name="width" id="width">
@@ -116,8 +121,11 @@ def index():
       <label for="purpose">용도:</label>
       <input type="text" name="purpose" id="purpose">
 
-      <label for="image">사진 첨부:</label>
-      <input type="file" name="image" id="image">
+      <label for="image1">파일 첨부 1(STL, STEP 등 3D 설계파일):</label>
+      <input type="file" name="image1" id="image1" required>
+
+      <label for="image2">파일 첨부 2(JPG, PNG 등 파트 사진 - 선택사항):</label>
+      <input type="file" name="image2" id="image2">
 
       <input type="submit" value="제출">
     </form>
@@ -135,36 +143,54 @@ def submit():
     weight = request.form.get('weight')
     material = request.form.get('material')
     purpose = request.form.get('purpose')
-    image = request.files['image']
+    image1 = request.files['image1']
+    image2 = request.files.get('image2')
 
-    if image and allowed_file(image.filename):
-        filename = secure_filename(image.filename)
-        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    filename1 = secure_filename(image1.filename)
+    image1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
 
-        # Create Excel file and write data
-        wb = openpyxl.Workbook()
-        sheet = wb.active
-        sheet['A1'] = 'Width'
-        sheet['B1'] = 'Height'
-        sheet['C1'] = 'Depth'
-        sheet['D1'] = 'Weight'
-        sheet['E1'] = 'Material'
-        sheet['F1'] = 'Purpose'
-        sheet['G1'] = 'Image Filename'
-        sheet['A2'] = width
-        sheet['B2'] = height
-        sheet['C2'] = depth
-        sheet['D2'] = weight
-        sheet['E2'] = material
-        sheet['F2'] = purpose
-        sheet['G2'] = filename
+    filename2 = None
+    if image2 and allowed_file(image2.filename):
+        filename2 = secure_filename(image2.filename)
+        image2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
 
-        excel_filename = f"{filename}.xlsx"
-        wb.save(os.path.join(app.config['UPLOAD_FOLDER'], excel_filename))
 
-        return jsonify({'message': '양식이 제출되었습니다.'})
-    else:
-        return jsonify({'error': '올바른 이미지 파일을 첨부해야 합니다.'})
+    #if image and allowed_file(image.filename):
+    #filename = secure_filename(image.filename)
+    #image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # Create Excel file and write data
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet['A1'] = '가로(mm)'
+    sheet['B1'] = '세로(mm)'
+    sheet['C1'] = '높이(mm)'
+    sheet['D1'] = '현재 파트 무게(g)'
+    sheet['E1'] = '현재 소재'
+    sheet['F1'] = '용도'
+    sheet['G1'] = '파일명'
+    sheet['A2'] = width
+    sheet['B2'] = height
+    sheet['C2'] = depth
+    sheet['D2'] = weight
+    sheet['E2'] = material
+    sheet['F2'] = purpose
+    sheet['G2'] = filename1
+ 
+    if filename2:
+      sheet['H1'] = '파일명'
+      sheet['H2'] = filename2
+
+
+    excel_filename = f"{filename1}.xlsx"
+    wb.save(os.path.join(app.config['UPLOAD_FOLDER'], excel_filename))
+
+    return '양식이 제출되었습니다.'
+
+   # return jsonify({'message': '양식이 제출되었습니다.'})
+
+   # else:
+   #     return jsonify({'error': '올바른 이미지 파일을 첨부해야 합니다.'})
 
 @app.route('/uploads')
 def list_uploads():
